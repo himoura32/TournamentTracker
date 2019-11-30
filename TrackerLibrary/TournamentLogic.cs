046dfc1bd5942717866d620ccf757d2b9c331cd4
himoura <fallingout31@gmail.com>
2019-11-30 17:58:18 -0500

    Lesson 18 done

--- TrackerLibrary/TournamentLogic.cs
+++ TrackerLibrary/TournamentLogic.cs
@@ -19,11 +19,61 @@ namespace TrackerLibrary
             List<TeamModel> randomizedTeams = RamdomizeTeamOrder(model.EnteredTeams);
             int rounds = FindNumberOfRounds(randomizedTeams.Count);
             int byes = NumberOfByes(rounds, randomizedTeams.Count);
+
+            model.Rounds.Add(CreateFirstRound(byes, randomizedTeams));
+
+            CreateOtherRounds(model, rounds);
+        }
+
+        private static void CreateOtherRounds(TournamentModel model, int rounds)
+        {
+            int round = 2;
+            List<MatchupModel> previousRound = model.Rounds[0];
+            List<MatchupModel> currRound = new List<MatchupModel>();
+            MatchupModel currMatchup = new MatchupModel();
+
+            while (round <= rounds)
+            {
+                foreach (MatchupModel match in previousRound)
+                {
+                    currMatchup.Entries.Add(new MatchUpEntryModel { ParentMatchup = match});
+
+                    if (currMatchup.Entries.Count > 1)
+                    {
+                        currMatchup.MatchUpRound = round;
+                        currRound.Add(currMatchup);
+                        currMatchup = new MatchupModel();
+                    }
+                }
+                model.Rounds.Add(currRound);
+                previousRound = currRound;
+                currRound = new List<MatchupModel>();
+                round += 1;
+            }
         }
 
         private static List<MatchupModel> CreateFirstRound(int byes, List<TeamModel> teams)
         {
+            List<MatchupModel> output = new List<MatchupModel>();
+            MatchupModel curr = new MatchupModel();
 
+            foreach (TeamModel team in teams)
+            {
+                curr.Entries.Add(new MatchUpEntryModel { TeamCompeting = team });
+
+                if (byes > 0 || curr.Entries.Count > 1)
+                {
+                    curr.MatchUpRound = 1;
+                    output.Add(curr);
+                    curr = new MatchupModel();
+
+                    if (byes > 0)
+                    {
+                        byes -= 1;
+                    }
+                }
+            }
+            return output;
         }
 
         private static int NumberOfByes(int rounds, int numberOfTeams)
